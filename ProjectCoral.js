5da547a2d7e9990d48f881ea06f6380d616f5c5d
Gtt1229 <gman1229@gmail.com>
2025-01-06 21:09:46 -0500

    Initial
    **Description**
    * Only controller support has been tested
    * Uses Goldberg and Nemirting
    * Works for controllers (Joining the lobby is difficult)
    * No 2nd player profile saves
    * Steam must be closed to prevent controller stealing
    
    **Issues**
    * Navigating the menu is difficult as the simulated cursor does not show, so navigating is unintuitive
    * If the emulated Steam Overlay is launched, you will not be able to close the overlay.

---
+++ ProjectCoral.js
@@ -0,0 +1,132 @@
+Hub.Handler.Version = 7; // Released at https://hub.splitscreen.me/ on Sat Jan 06 2024 08:13:00 GMT+0000 (Coordinated Universal Time).
+
+Game.DirSymlinkExclusions = ["Engine\\Binaries\\ThirdParty\\Steamworks\\Steamv142\\Win64", "ProjectCoral\\Binaries\\Win64"];
+Game.FileSymlinkExclusions = ["steam_api64.dll", "steam_appid.txt", "winmm.dll", "EOSSDK-Win64-Shipping.dll"];
+Game.NeedsSteamEmulation = false;
+Game.UseGoldberg = true;
+Game.GoldbergExperimental = true;
+Game.GoldbergLobbyConnect = false;
+Game.UseSteamStubDRMPatcher = false;
+Game.CreateSteamAppIdByExe = true;
+Game.HandlerInterval = 100;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = false;
+Game.ExecutableName = "ProjectCoral-Win64-Shipping.exe";
+Game.SteamID = "1158160";
+Game.GUID = "Coral Island";
+Game.GameName = "Coral Island";
+Game.MaxPlayers = 4;
+Game.MaxPlayersOneMonitor = 4;
+Game.BinariesFolder = "ProjectCoral\\Binaries\\Win64";
+Game.LauncherTitle = "";
+Game.HideTaskbar = false;
+Game.Hook.ForceFocus = true;
+Game.Hook.ForceFocusWindowName = "Coral Island";
+Game.SetWindowHook = true;
+Game.ResetWindows = true;
+Game.RefreshWindowAfterStart = true;
+Game.SetForegroundWindowElsewhere = true;
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = false;
+Game.Hook.XInputReroute = false;
+Game.XInputPlusDll = ["xinput1_3.dll"];
+Game.Hook.CustomDllEnabled = false;
+Game.SupportsKeyboard = true;
+Game.UserProfileSavePath = "AppData\\Local\\ProjectCoral\\Saved\\SaveGames";
+Game.UserProfileSavePathNoCopy = true;
+Game.Description =
+  "Create a Co-op game as normal. Press start, go to Party Management, carefully navigate to view the invite code (down, right). Second player join via invite code (Keyboard is trial and error due to the inability to see cursor position";
+Game.PauseBetweenStarts = 10;
+Game.UseNemirtingasEpicEmu = true;
+
+
+
+
+//ProtoInput
+// This can be true or false, whether or not you want to allow raw keyboards/mice to be selected in the Nucleus GUI. Doesn't actually affect the hooks being injected or not.
+Game.SupportsMultipleKeyboardsAndMice = true;
+
+// These options are deprecated, and must explicitly be set to false (since some default to true)
+Game.HookSetCursorPos = false;
+Game.HookGetCursorPos = false;
+Game.HookGetKeyState = false;
+Game.HookGetAsyncKeyState = false;
+Game.HookGetKeyboardState = false;
+Game.HookFilterRawInput = false;
+Game.HookFilterMouseMessages = false;
+Game.HookUseLegacyInput = false;
+Game.HookDontUpdateLegacyInMouseMsg = false;
+Game.HookMouseVisibility = false;
+Game.SendNormalMouseInput = false;
+Game.SendNormalKeyboardInput = false;
+Game.SendScrollWheel = false;
+Game.ForwardRawKeyboardInput = false;
+Game.ForwardRawMouseInput = false;
+Game.HookReRegisterRawInput = false;
+Game.HookReRegisterRawInputMouse = false;
+Game.HookReRegisterRawInputKeyboard = false;
+Game.DrawFakeMouseCursor = false;
+
+// Set which injection method you want to use
+Game.ProtoInput.InjectStartup = true;
+Game.ProtoInput.InjectRuntime_RemoteLoadMethod = true;
+Game.ProtoInput.InjectRuntime_EasyHookMethod = true;
+Game.ProtoInput.InjectRuntime_EasyHookStealthMethod = false;
+
+// Automatically locks input once the instances are set up
+Game.LockInputAtStart = false;
+
+// This should almost always be on (see above)
+Game.LockInputSuspendsExplorer = true;
+
+// This is identical to the setting in ProtoInputHost. You should leave this on so the "fake" input is only sent when the "real" input isn't
+Game.ProtoInput.FreezeExternalInputWhenInputNotLocked = true;
+
+// Sets the virtual keycode for locking input (End key by default)
+Game.LockInputToggleKey = 0x23;
+
+////Game.ProtoInput.RegisterRawInputHook = true;
+////Game.ProtoInput.GetRawInputDataHook = true;
+////Game.ProtoInput.MessageFilterHook = true;
+Game.ProtoInput.GetCursorPosHook = true;
+Game.ProtoInput.SetCursorPosHook = true;
+Game.ProtoInput.GetKeyStateHook = true;
+Game.ProtoInput.GetAsyncKeyStateHook = true;
+Game.ProtoInput.GetKeyboardStateHook = true;
+Game.ProtoInput.CursorVisibilityHook = true;
+Game.ProtoInput.ClipCursorHook = true;
+Game.ProtoInput.ClipCursorHookCreatesFakeClip = true;
+Game.ProtoInput.FocusHooks = true;
+
+Game.ProtoInput.XinputHook = true;
+Game.ProtoInput.UseOpenXinput = true;
+Game.ProtoInput.UseDinputRedirection = true;
+Game.Hook.DInputEnabled = false;
+Game.ProtoInput.RawInputFilter = true;
+
+Game.ProtoInput.MouseActivateFilter = true;
+Game.ProtoInput.WindowActivateFilter = true;
+Game.ProtoInput.WindowActvateAppFilter = true;
+Game.ProtoInput.MouseWheelFilter = true;
+
+////Game.ProtoInput.MouseButtonFilter = true;
+////Game.ProtoInput.KeyboardButtonFilter = true;
+
+Game.ProtoInput.BlockedMessages = [ 0x0008 ];
+
+Game.ProtoInput.EnableFocusMessageLoop = true;
+Game.ProtoInput.FocusLoopIntervalMilliseconds = 5;
+Game.ProtoInput.FocusLoop_WM_ACTIVATE = true;
+Game.ProtoInput.FocusLoop_WM_SETFOCUS = true;
+Game.ProtoInput.FocusLoop_WM_MOUSEACTIVATE = true;
+Game.ProtoInput.FocusLoop_WM_ACTIVATEAPP = false;
+Game.ProtoInput.FocusLoop_WM_NCACTIVATE = false;
+
+
+
+
+
+Game.Play = function() {
+  Context.StartArguments = " -windowed -AlwaysFocus -nosplash " + " -ResX=" + Context.Width + " -ResY=" + Context.Height;
+};
